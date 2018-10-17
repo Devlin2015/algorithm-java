e7d79bb00f722888da39a7b3cce941de08584b40
devlin2015 <devlin2015@163.com>
2018-10-17 09:19:02 +0800

    Initial commit

---
+++ src/main/java/com/hq/algorithmjava/AlgorithmJavaApplication.java
@@ -0,0 +1,12 @@
+package com.hq.algorithmjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmJavaApplication.class, args);
+	}
+}
